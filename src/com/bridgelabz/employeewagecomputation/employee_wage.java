7cd43393afdda16af749c0383ca3942093baacad
mk3709 <mk3709@srmist.edu.in>
2024-01-18 23:05:53 +0530

    New_Part_time_added

--- src/com/bridgelabz/employeewagecomputation/employee_wage.java
+++ src/com/bridgelabz/employeewagecomputation/employee_wage.java
@@ -2,10 +2,11 @@ package com.bridgelabz.employeewagecomputation;
 
 public class employee_wage {
         public static void main(String[] args) {
-
 // Constants
 
-                int IS_FULL_TIME = 1;
+                int IS_PART_TIME = 1;
+
+                int IS_FULL_TIME = 2;
 
                 int EMP_RATE_PER_HOUR = 20;
 
@@ -17,9 +18,13 @@ public class employee_wage {
 
 // Computation
 
-                double empCheck = Math.floor(Math.random() * 10) % 2;
+                double empCheck = Math.floor(Math.random()*10)%3;
 
-                if (empCheck == IS_FULL_TIME)
+                if (empCheck == IS_PART_TIME)
+
+                    empHrs = 4;
+
+                else if (empCheck == IS_FULL_TIME)
 
                     empHrs = 8;
 
@@ -27,9 +32,7 @@ public class employee_wage {
 
                     empHrs = 0;
 
-                empWage = empHrs * EMP_RATE_PER_HOUR;
-
-                System.out.println("Emp Wage: "+ empWage);
+                empWage = empHrs * EMP_RATE_PER_HOUR; System.out.println("Emp Wage: " + empwage);
 
             }
 
