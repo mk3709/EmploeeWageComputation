878f2a8e46cb3b86011747434359be6e91546d31
mk3709 <mk3709@srmist.edu.in>
2024-01-18 23:02:18 +0530

    Employee_daily_wage_added

--- src/com/bridgelabz/employeewagecomputation/employee_wage.java
+++ src/com/bridgelabz/employeewagecomputation/employee_wage.java
@@ -5,21 +5,33 @@ public class employee_wage {
 
 // Constants
 
-            int IS_FULL_TIME = 1;
+                int IS_FULL_TIME = 1;
+
+                int EMP_RATE_PER_HOUR = 20;
+
+// Variables
+
+                int empHrs = 0;
+
+                int empWage = 0;
 
 // Computation
 
-            double empCheck = Math.floor(Math.random() * 10) % 2;
+                double empCheck = Math.floor(Math.random() * 10) % 2;
 
-            if (empCheck == IS_FULL_TIME)
+                if (empCheck == IS_FULL_TIME)
 
-                System.out.println("Employee is Present");
+                    empHrs = 8;
 
-            else
+                else
 
-                System.out.println("Employee is Abscent");
+                    empHrs = 0;
+
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+
+                System.out.println("Emp Wage: "+ empWage);
+
+            }
 
         }
-
-    }
 
