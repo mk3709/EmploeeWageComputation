55087f52d8a0d52cf4052b4776fe62db03a05f61
mk3709 <mk3709@srmist.edu.in>
2024-01-18 22:51:08 +0530

    Feauture Added :Attendance

---
+++ src/com/bridgelabz/employeewagecomputation/employee_wage.java
@@ -0,0 +1,25 @@
+package com.bridgelabz.employeewagecomputation;
+
+public class employee_wage {
+        public static void main(String[] args) {
+
+// Constants
+
+            int IS_FULL_TIME = 1;
+
+// Computation
+
+            double empCheck = Math.floor(Math.random() * 10) % 2;
+
+            if (empCheck == IS_FULL_TIME)
+
+                System.out.println("Employee is Present");
+
+            else
+
+                System.out.println("Employee is Abscent");
+
+        }
+
+    }
+
