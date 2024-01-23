a7dfec4ddb5ec8f49c14f3e31517a950ba091941
mk3709 <mk3709@srmist.edu.in>
2024-01-23 14:34:13 +0530

    UC8 Comapnies added using Class Method

--- src/com/bridgelabz/employeewagecomputation/employee_wage.java
+++ src/com/bridgelabz/employeewagecomputation/employee_wage.java
@@ -2,62 +2,36 @@ package com.bridgelabz.employeewagecomputation;
 
 public class employee_wage {
     public static final int IS_PART_TIME = 1;
-
     public static final int IS_FULL_TIME = 2;
 
-    public static final int EMP_RATE_PER_HOUR = 20;
-
-    public static final int NUM_OF_WORKING_DAYS = 2;
-
-    public static final int MAX_HRS_IN_MONTH= 10;
-
- public static int computeEmpWage(){
-
-// Variables
-
-
-        int empHrs = 0, totalEmpHrs=0, totalWorkingDays = 0;
-
-
-
-// Computation
-
-        while ( totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
-        {
-
+    public static int  computeEmpWage(String company ,int EMP_RATE_PER_HOUR,int NUM_OF_WORKING_DAYS,int MAX_HRS_IN_MONTH) {
+        int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+        while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
             totalWorkingDays++;
-
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-
-            switch(empCheck) {
-
+            switch (empCheck) {
                 case IS_PART_TIME:
-
                     empHrs = 4;
                     break;
-
                 case IS_FULL_TIME:
-
                     empHrs = 8;
-
                     break;
-
                 default:
-
                     empHrs = 0;
             }
             totalEmpHrs += empHrs;
-
-            System.out.println("Day#:" + totalWorkingDays + "Emp Hr:"+empHrs);
-
+            System.out.println("Day#:" + totalWorkingDays + "Emp Hr:" + empHrs);
         }
         int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-
         System.out.println("Total Emp Wage: " + totalEmpWage);
         return totalEmpWage;
     }
     public static void main(String[] args)
     {
-        computeEmpWage();
+
+        computeEmpWage("Dmart",20,2,10);
+        computeEmpWage("Reliance",10,4,20);
+
     }
+
 }
