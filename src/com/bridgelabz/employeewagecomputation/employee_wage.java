e1e0177ca974f9454d8ec499211df28dcd9225ab
mk3709 <mk3709@srmist.edu.in>
2024-01-23 14:18:43 +0530

    UC7_Class_is_aded

--- src/com/bridgelabz/employeewagecomputation/employee_wage.java
+++ src/com/bridgelabz/employeewagecomputation/employee_wage.java
@@ -7,25 +7,29 @@ public class employee_wage {
 
     public static final int EMP_RATE_PER_HOUR = 20;
 
-    public static final int NUM_OF_WORKING_DAYS = 2; public static final int MAX_HRS_IN_MONTH= 10;
+    public static final int NUM_OF_WORKING_DAYS = 2;
 
-    public static void main(String[] args) {
+    public static final int MAX_HRS_IN_MONTH= 10;
+
+ public static int computeEmpWage(){
 
 // Variables
 
-        int empHrs = 0, totalEmpHrs, totalWorkingDays = 0;
+
+        int empHrs = 0, totalEmpHrs=0, totalWorkingDays = 0;
 
 
 
 // Computation
 
-        while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+        while ( totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+        {
 
             totalWorkingDays++;
 
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 
-            switch (empCheck) {
+            switch(empCheck) {
 
                 case IS_PART_TIME:
 
@@ -44,11 +48,16 @@ public class employee_wage {
             }
             totalEmpHrs += empHrs;
 
-            System.out.println("Day#:" + totalworkingDays + "Emp Hr:"+empHrs);
+            System.out.println("Day#:" + totalWorkingDays + "Emp Hr:"+empHrs);
 
         }
-        int totalEmpWage = total_Empirs * EMP_RATE_PER_HOUR;
+        int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
 
         System.out.println("Total Emp Wage: " + totalEmpWage);
+        return totalEmpWage;
     }
+    public static void main(String[] args)
+    {
+        computeEmpWage();
     }
+}
