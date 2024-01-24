5eb866ea5f367b4d5e16aa77f794b7fff7ba603d
mk3709 <mk3709@srmist.edu.in>
2024-01-24 10:42:32 +0530

    UC11_API_Added

--- src/com/bridgelabz/employeewagecomputation/CompanyEmployeeWage.java
+++ src/com/bridgelabz/employeewagecomputation/CompanyEmployeeWage.java
@@ -1,7 +1,6 @@
 package com.bridgelabz.employeewagecomputation;
 
 public class CompanyEmployeeWage {
-
     public final String company;
     public final int EMP_RATE_PER_HOUR;
     public final int NUM_OF_WORKING_DAYS;
@@ -13,19 +12,17 @@ public class CompanyEmployeeWage {
         this.EMP_RATE_PER_HOUR = EMP_RATE_PER_HOUR;
         this.NUM_OF_WORKING_DAYS = NUM_OF_WORKING_DAYS;
         this.MAX_HRS_IN_MONTH = MAX_HRS_IN_MONTH;
-
     }
-
 
     public void setTotalEmpWage(int totalEmpWage) {
         this.totalEmpWage = totalEmpWage;
     }
 
-
     @Override
     public String toString() {
-        return "employee_wage_pract" + "totalEmpWage=" + totalEmpWage;
+        return "CompanyEmployeeWage{" +
+                "company='" + company + '\'' +
+                ", totalEmpWage=" + totalEmpWage +
+                '}';
     }
 }
-
-
